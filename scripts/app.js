05946e4b2acbca6f15d56d2406e134d7ca33df3d
Egbetokun Timilehin <timmyaof02@gmail.com>
2024-03-06 09:46:38 +0100

    Worked on the nav links responsiveness

--- scripts/app.js
+++ scripts/app.js
@@ -11,3 +11,21 @@ function preloader() {
 }
 
 setInterval(() => preloader(), 500);
+
+// Nav Link
+const menu_bar = document.querySelector(".menu");
+const close_bar = document.querySelector(".close");
+const nav_link = document.querySelector(".nav_link")
+
+menu_bar.addEventListener("click", ()=>{
+      nav_link.style.display = "flex";
+      nav_link.style.animation = "slide_in 0.5s"
+      nav_link.style.width = "70%";
+})
+
+
+close_bar.addEventListener("click", ()=>{
+  nav_link.style.animation = "slideOut 0.5s";
+  nav_link.style.width = "0%";
+  nav_link.style.display = "none";
+})
